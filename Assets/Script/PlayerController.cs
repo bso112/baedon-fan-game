9cd4524b17ede8de280a0c328f1c13fdeabbf3bd
byeon <bso11246@gmail.com>
2021-02-07 18:59:21 +0900

    Add movement

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController), typeof(Animator))]
+public class PlayerController : MonoBehaviour
+{
+    private const float comboTolerance = 0.5F;
+    private const float GRAVITY_VALUE = -9.81f;
+
+    private CharacterController charController;
+    private Animator animator;
+
+    private float currentComboAcc = 0.0F;
+    private Vector3 playerVelocity;
+    private bool isGround;
+    private float playerSpeed = 2.0f;
+    private float jumpHeight = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        charController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkIsGround();
+
+        tryPlayAniamtion();
+
+        tryMove();
+
+        tryJump();
+
+        updateValue();
+    }
+
+    void tryPlayAniamtion()
+    {
+
+        currentComboAcc += Time.deltaTime;
+
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+
+            if (currentComboAcc < comboTolerance)
+            {
+                animator.SetTrigger("is_combo");
+                currentComboAcc = 0F;
+                return;
+            }
+
+            animator.SetTrigger("basic_combo");
+            currentComboAcc = 0F;
+
+        }
+
+        animator.SetFloat("horizontal", Input.GetAxis("Horizontal"));
+        animator.SetFloat("vertical", Input.GetAxis("Vertical"));
+
+    }
+
+
+    void checkIsGround()
+    {
+        isGround = charController.isGrounded;
+        if (isGround && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+    }
+
+    void tryMove()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        charController.Move(move * Time.deltaTime * playerSpeed);
+
+/*        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+*/
+    }
+
+    void tryJump()
+    {
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && isGround)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * GRAVITY_VALUE);
+        }
+    }
+
+    void updateValue()
+    {
+        playerVelocity.y += GRAVITY_VALUE * Time.deltaTime;
+        charController.Move(playerVelocity * Time.deltaTime);
+    }
+}
