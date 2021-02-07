5195f544421ebf929e94c290c2ba2e4e59aedef5
byeon <bso11246@gmail.com>
2021-02-07 10:16:58 +0900

    init

---
+++ Assets/PlayerCamController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamController : MonoBehaviour
+{
+    public Transform mTarget;
+
+    public Vector3 mOffset;
+    public float mZoomSpeed = 400f;
+    public float mMinZoom = 5f;
+    public float mMaxZoom = 15f;
+    public float mYawSpeed = 100f;
+
+    public float mCurrentZoom = 8f;
+    public float mCurrentYaw = 0f;
+    public float mPitch = 2f;
+
+
+    private void Update()
+    {
+        mCurrentZoom -= Input.GetAxis("Mouse ScrollWheel") * mZoomSpeed * Time.deltaTime;
+        mCurrentZoom = Mathf.Clamp(mCurrentZoom, mMinZoom, mMaxZoom);
+        mCurrentYaw -= Input.GetAxis("Mouse X") * mYawSpeed * Time.deltaTime;
+    }
+
+
+    void LateUpdate()
+    {
+        transform.position = mTarget.position + mOffset * mCurrentZoom;
+        transform.LookAt(mTarget.position + Vector3.up * mPitch);
+        transform.RotateAround(mTarget.position, Vector3.up, mCurrentYaw);
+    }
+}
